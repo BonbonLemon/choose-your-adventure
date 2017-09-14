da3af516cce0dfb39996d023581f0921fc0811f9
Stephen Chen <chen.stephen.r@gmail.com>
2017-09-14 12:28:05 -0700

    authorization for user done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,21 +2,30 @@ class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true, uniqueness: true
   validates :password, length: {minimum: 6, allow_nil: true}
   validates :username, length: {maximum: 20}
-  # after_initialize :ensure_session_token
+  before_validation :ensure_session_token
+
+  attr_reader :password
 
   def is_password?(password)
-
+    BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
   def password=(password)
-
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
 
+  def generate_session_token
+    SecureRandom.urlsafe_base64
   end
 
   def ensure_session_token
-
+    self.session_token ||= generate_session_token
   end
 end
