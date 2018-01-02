c57c3ba9f204b616c0deb0cf1e6e792dc376e117
Stephen Chen <chen.stephen.r@gmail.com>
2018-01-02 15:02:20 -0800

    created some reducers for session

---
+++ frontend/choose_your_adventure.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import Root from './components/root';
+// import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('hello');
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to the site</h1>, root);
+});
